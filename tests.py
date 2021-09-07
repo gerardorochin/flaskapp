a7119119d593804554a75333fcd53d6a76157211
Gerardo Rochín <gerardorochin@gmail.com>
2021-09-07 10:54:31 -0600

    Update tests.py

--- tests.py
+++ tests.py
@@ -4,7 +4,7 @@ import unittest
 class TestSum(unittest.TestCase):
 
     def test_sum(self):
-        self.assertEqual(sum([2, 3, 6]), 10, "Deberia ser 10")
+        self.assertEqual(sum([2, 3, 5]), 10, "Deberia ser 10")
 
 if __name__ == '__main__':
     unittest.main()
